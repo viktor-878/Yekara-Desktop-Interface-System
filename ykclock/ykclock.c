46be58b943c9e813b57ba3b4cebfd3b35121ac73
viktor878 <victorgabrielnet53@gmail.com>
2026-06-25 09:25:42 -0300

    update cursor images

--- ykclock/ykclock.c
+++ ykclock/ykclock.c
@@ -75,7 +75,7 @@
 #define MONTH_X_POS		10
 #define MONTH_Y_POS		3
 #define MONTH_X_OFFSET		0
-#define MONTH_WIDTH		22
+#define MONTH_WIDTH		26
 #define MONTH_HEIGHT		6
 
 #define DATE_LEFT_X_POS		7
@@ -146,7 +146,7 @@ int enableShapedWindow = 1;	/* default value is noshape */
 int enableBlinking = 1;		/* default is blinking */
 int startIconified = 0;		/* default is not iconified */
 int enableYearDisplay = 0;	/* default is to show time, not year */
-unsigned int blinkInterval = 2;          /* default is a 2-second blink cycle */
+unsigned int blinkInterval = 1;          /* default is a 2-second blink cycle */
 
 int timePos12[NUM_TIME_POSITIONS]  = { 5, 14, 24, 28, 37 };
 int timePos24[NUM_TIME_POSITIONS]  = { 4,  8, 17, 22, 31 };
